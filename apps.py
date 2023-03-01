3af6bead915e97366a705e2deb2523705e47ce06
Ubuntu <ubuntu@ip-172-31-65-34.ec2.internal>
2023-03-01 05:45:23 +0000

    Creación archivo apps.py y requirements.txt

---
+++ apps.py
@@ -0,0 +1,23 @@
+"""
+Your module description
+"""
+import json
+from urllib.request import urlopen
+import boto3
+
+def f(event, context):
+    
+    with urlopen("https://totoro.banrep.gov.co/estadisticas-economicas/rest/consultaDatosService/consultaMercadoCambiario") as response:
+        body = response.read()
+        
+    print(body)
+
+    s3 = boto3.resource('s3')
+    object = s3.Object('zappa-tmtvhlyzs', 'dolar_timestamp.txt')
+    object.put(Body=body)
+    
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
